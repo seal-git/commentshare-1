e20d9266b8692c747a6200c865444b45186b0d7b
seal0 <seal0@DESKTOP-KB712J3>
2020-07-03 04:56:23 +0900

    コメントのオーバーレイ

--- commentshare/static/js/overlay_comment.js
+++ commentshare/static/js/overlay_comment.js
@@ -3,11 +3,74 @@ $(window).on('load',function(){
 	console.log("overlay")
 })
 
-const a = document.getElementById("viewer");
+var commentList = [
+{"value": "コメント1", "span-page":2, "span-left": 219, "span-top": 711, "offset": -200},
+{"value": "コメント2", "span-page":2, "span-left": 251, "span-top": 623, "offset": 0},
+{"value": "コメント3", "span-page":2, "span-left": 251, "span-top": 623, "offset": 50}
+];
 
-a.onmouseover = function(){
+const a = document.getElementById("viewer");
+a.onmousemove =function(){
+	var hover = $(":hover");
+	Object.keys(hover).forEach(function(key){	//連想配列をforEachするときの書き方(途中でbreakはできない)
+		if(hover[key].tagName == "SPAN"){
+			hover[key].onmousemove = function(){
+				//要素のcssスタイル、ページ番号を取得
+				var style = window.getComputedStyle(hover[key]);
+				var top = style.getPropertyValue("top").slice(0, -2);
+				top = parseInt(top, 10);
+				var left = style.getPropertyValue("left").slice(0,-2);
+				left = parseInt(left, 10);
+				var page = hover[key].parentNode.parentNode.dataset.pageNumber;
+				console.log(hover[key].textContent);
+				console.log("left", left, "top", top, "page", page);
+
+				var target = commentList.filter(function(comment){
+					console.log(comment["span-left"]);
+					return(comment["span-left"] === left)
+				});
+				target = target.filter(function(comment){
+					return(comment["span-top"] === top)
+				});
+				console.log("target", target[0]);
+				if(target.length){
+					var commentTarget = target[0];
+					//要素の背景色の変更
+					hover[key].style.backgroundColor = "red";
+
+					//吹き出しを表示(jquery.balloon.jsから)
+					$(hover[key]).balloon({
+						position: "right",
+						offsetX : commentTarget["offset"],
+						showDuration: 10,
+						tipSize: 20,
+						css: {
+						"color": "#0000ff",
+						"font-size": "20px",
+						"font-weight": "bold",
+						"border": "solid 2px #111",
+						"padding": "0px",
+						"background-color": "#eee",
+						"opacity": 1,
+						},
+						"html":true,
+						contents: makeComment(commentTarget.value)
+					});
+				}
+
+			}
+			hover[key].onmouseleave = function(){
+					hover[key].style.backgroundColor = "white"
+			}
+		}
+	});
+}
+
+const b = document.getElementById("viewer");
+
+// a.onmouseover = function(){
 	var pages = a.getElementsByClassName("page");
-	// console.log(pages.item(0).dataset.pageNumber);
+	console.log(pages.item(0).dataset.pageNumber);
 	var page1;
 	for(var i=0;i<pages.length;i++){
 		if(pages.item(i).dataset.pageNumber=="1"){
@@ -16,19 +79,19 @@ a.onmouseover = function(){
 			break;
 		}
 	}
-	console.log(page1);
-	console.log(page1.getElementsByClassName("textLayer").item(0).getElementsByTagName("span").item(1).textContent);
-	console.log(page1.getElementsByClassName("textLayer").item(0).getElementsByTagName("span").item(1).textContent);
-	function overlay(event){
-		console.log("cmment")
-		console.log(event.target.textContent);
-	}
+	// console.log(page1);
+	// console.log(page1.getElementsByClassName("textLayer").item(0).getElementsByTagName("span").item(1).textContent);
+
 	var chars = page1.getElementsByTagName("span");
 	for(var i=0;i<chars.length;i++){
-		console.log(chars[i]);
-		chars[i].onmouseover = overlay();
-	}
+		// console.log(chars[i]);
+		chars[i].onmouseover = function(event){
+		console.log("cmment")
+		console.log(event.target.textContent);
+		console.log(event.target);
+		}
+	};
 
 
-}
+// }
 
---
+++ commentshare/static/js/send_comment.js
@@ -0,0 +1,114 @@
+/**
+ *
+ */
+
+
+//なにしてるのかよく分からん。文法的に正しいのか？
+//window.onload = function(){
+//
+//	document.getElementsByTagName.onselect = function(){
+//		console.log('2');
+//		getText();
+//	};
+//}
+
+function getText(){
+	//選択範囲のノードを返す
+	//選択範囲がなかったり広すぎたら0を返す
+	var Selected = window.getSelection()
+	var SelectedText = Selected.toString();
+	if(Selected.anchorNode.parentElement.className=="balloon"){
+		return 0;
+	}else if(SelectedText.length<1||SelectedText.length>15){
+		return 0;
+	}else{
+		console.log(SelectedText);
+		// console.log(Selected);
+		return Selected;
+	}
+}
+
+
+window.onclick = function() {
+	var Selected = this.getText();
+
+	if(Selected==0){
+		//選択されてない状態でフォーム以外がクリックされたらフォームが消える
+
+		var hover = $(":hover");	//現在カーソルがかかっている要素を返す
+		var flag = true;	//カーソルがフォームにかかっていなかったらtrue
+		this.Object.keys(hover).forEach(function(key){	//連想配列をforEachするときの書き方(途中でbreakはできない)
+			// console.log(hover[key])
+			if(hover[key].id == "comment-form"){
+				flag = false;
+			}
+		});
+		//カーソルがフォームにかかっていなかったらフォームを消す
+		if(flag){
+			var o = document.getElementById("comment-form");
+			if(o){
+				document.getElementById("viewerContainer").removeChild(o);
+			}
+		}
+	}else{
+		//既にフォームがあったら削除する
+		var o = document.getElementById("comment-form");
+		if(o){
+			document.getElementById("viewerContainer").removeChild(o);
+		}
+
+		//選択されたノードの座標を取得
+		var clientRect = Selected.focusNode.parentElement.getBoundingClientRect();
+		console.log(clientRect.left, clientRect.top);
+		console.log(Selected);
+		console.log(Selected.anchorNode);
+
+		//入力フォームフィールドを作成してviewerContainerの下に入れる
+		form = document.createElement("form");
+		form.setAttribute("id","comment-form");
+		form.style.position = "fixed";
+		form.style.top = (clientRect.top-20)+"px";
+		form.style.left = (clientRect.left+20)+"px";
+		document.getElementById("viewerContainer").appendChild(form);
+
+		//入力フォームを作成してformの下に入れる
+		o = document.createElement("input");
+		o.setAttribute("type","text");
+		o.setAttribute("id","comment-input");
+		document.getElementById("comment-form").appendChild(o);
+
+		//送信ボタンを作成してformの下に入れる
+		p = document.createElement("input");
+		p.setAttribute("type","submit");
+		p.setAttribute("value", "送信")
+		p.setAttribute("id","comment-submit");
+		p.style = "WIDTH:50px; HEIGHT:20px"
+		document.getElementById("comment-form").appendChild(p);
+
+		//コメント情報をjsonにしてサーバに送信する
+		form.onsubmit=function(){
+			// alert(o.value+"\n"+clientRect.left+","+ clientRect.top);
+			// alert(clientRect.left+","+ clientRect.top);
+			var data = [
+				{"name" : "test_user"},
+				{"comment" : o.value},
+				// {"node" : Selected},
+				{"time" : Date.now()}
+			]
+			var json_data = JSON.stringify(data);
+			const xhr = new XMLHttpRequest();
+			xhr.open("POST", "/add_comment");
+			xhr.setRequestHeader("Content-Type", "application/json")
+			xhr.send(json_data);
+			// console.log(data["node"]);
+			o.value = "";
+			document.getElementById("viewerContainer").removeChild(form);
+		}
+
+
+
+	}
+}
+
+
+
