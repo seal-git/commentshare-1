a581150b9564542c51e89648615aa9e3136f5217
yuki yoshinari <seal0828.4477@gmail.com>
2021-08-29 22:15:53 +0900

    update

--- flask/app/models.py
+++ flask/app/models.py
@@ -1,5 +1,6 @@
 from app import app_, db_, login_manager
 from app.db_define import User
+from flask import request, jsonify
 
 
 
@@ -8,3 +9,8 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+@app_.route('/api_test', methods=['POST'])
+def api_test():
+    json_data = request.json
+    print(json_data)
+    return "success"
